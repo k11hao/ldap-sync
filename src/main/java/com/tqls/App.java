154d8a31e3f8decb8cf693cc65fcf436e54db645
oyjg@tqlsgroup.com <jianguo88>
2022-01-24 15:22:25 +0800

    first commit

---
+++ src/main/java/com/tqls/App.java
@@ -0,0 +1,108 @@
+package com.tqls;
+
+import com.unboundid.ldap.sdk.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws ParseException {
+
+//        long period  = 60*60*60*1000;
+      long period  = 2*60*60*1000;//2小时
+        final String date = null;
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                sync(date);
+            }
+        }, 1000,period);
+
+
+    }
+
+    public static void login() {
+        LDAPAuthentication ldapAuthentication = new LDAPAuthentication();
+        ldapAuthentication.authenricate("025032", "");
+    }
+
+    public static void sync(String dateStr)  {
+
+//        List<SearchResultEntry> result = new ArrayList<>();
+        try {
+
+            String dn = "ou=Internal,ou=People,dc=tqls,dc=cn";
+            Calendar c = Calendar.getInstance();
+            if (dateStr != null && !dateStr.equals("")) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+                Date date = sdf.parse(dateStr);
+                c.setTime(date);
+            }
+            //设置成0点,也就是查1天的
+            c.set(Calendar.HOUR_OF_DAY,0);
+            SimpleDateFormat sdf_ldap = new SimpleDateFormat("yyyyMMddHHmmss");
+            String syncDate = sdf_ldap.format(c.getTime()) + "Z";
+            Filter filter = Filter.createGreaterOrEqualFilter("modifytimestamp", syncDate);
+
+
+            LDAPConnection connection_prd = new LDAPConnection("*", 1389, "cn=Directory Manager", "");
+
+            LDAPConnection connection = new LDAPConnection("*", 389, "cn=Directory Manager", "");
+
+            SearchScope scope = SearchScope.SUB;
+            /**
+             *
+             * LDAPSearchResults res = ld.search( MY_SEARCHBASE, LDAPConnection.SCOPE_SUB, MY_FILTER, new String[]{"*","createTimestamp","modifyTimestamp","creatorsName","modifiersName","subschemaSubentry"}, false );
+             */
+            SearchRequest searchRequest = new SearchRequest(dn, scope, filter, new String[]{"*", "createTimestamp", "modifyTimestamp", "creatorsName", "modifiersName"});
+            SearchResult searchResult = connection_prd.search(searchRequest);
+            System.out.println("查询结果数:" + searchResult.getEntryCount());
+            int updateCount = 0;
+            if (searchResult.getEntryCount() <= 0) {
+                return;
+            }
+            List<String> skip = Arrays.asList("createTimestamp", "modifyTimestamp", "creatorsName", "modifiersName");
+
+            for (SearchResultEntry searchResultEntry : searchResult.getSearchEntries()) {
+
+                Collection<Attribute> attributesOld = searchResultEntry.getAttributes();
+
+                List<Attribute> attributes = new ArrayList<>();
+
+                for (Attribute attribute : attributesOld) {
+                    if (skip.contains(attribute.getName())) continue;
+                    attributes.add(attribute);
+                }
+                //Attribute(name=modifyTimestamp, values={'20220119052735Z'})
+
+
+                Filter user_filter = Filter.createEqualityFilter("uid", searchResultEntry.getAttributeValue("uid"));
+                SearchRequest user_searchRequest = new SearchRequest(dn, scope, user_filter);
+                SearchResult user_searchResult = connection.search(user_searchRequest);
+                if (user_searchResult.getEntryCount() > 0) {
+                    //如果有要提前删除
+                    connection.delete(searchResultEntry.getDN());
+                }
+                try {
+                    LDAPResult result = connection.add(searchResultEntry.getDN(), attributes);
+                    System.out.println(searchResultEntry.getDN()+"更新结果"+result.getResultString());
+                    updateCount++;
+                } catch (Exception err) {
+                    System.out.println("更新失败:"+searchResultEntry.getDN());
+                    err.printStackTrace();
+
+                }
+
+            }
+            System.out.println("更新条数:"+updateCount);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/com/tqls/LDAPAuthentication.java
@@ -0,0 +1,91 @@
+package com.tqls;
+
+import java.util.Hashtable;
+
+import javax.naming.AuthenticationException;
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.Control;
+import javax.naming.ldap.InitialLdapContext;
+import javax.naming.ldap.LdapContext;
+
+public class LDAPAuthentication {
+    private final String URL = "ldap://192.168.50.135:1389/";
+    private final String BASEDN = "ou=People,dc=tqls,dc=cn";
+    private final String FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
+    private LdapContext ctx = null;
+    private final Control[] connCtls = null;
+
+    private void LDAP_connect() {
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, FACTORY);
+        env.put(Context.PROVIDER_URL, URL + BASEDN);
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+
+        String root = "cn=Directory Manager";// root
+        env.put(Context.SECURITY_PRINCIPAL, root);
+        env.put(Context.SECURITY_CREDENTIALS, "tqls");
+        // 此处若不指定用户名和密码,则自动转换为匿名登录
+        try {
+            ctx = new InitialLdapContext(env, connCtls);
+        } catch (javax.naming.AuthenticationException e) {
+            System.out.println("验证失败：" + e.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUserDN(String uid) {
+        String userDN = "";
+        LDAP_connect();
+        try {
+            SearchControls constraints = new SearchControls();
+            constraints.setSearchScope(SearchControls.SUBTREE_SCOPE);
+            NamingEnumeration<SearchResult> en = ctx.search("", "uid=" + uid, constraints);
+            if (en == null || !en.hasMoreElements()) {
+                System.out.println("未找到该用户");
+            }
+            // maybe more than one element
+            while (en != null && en.hasMoreElements()) {
+                Object obj = en.nextElement();
+                if (obj instanceof SearchResult) {
+                    SearchResult si = (SearchResult) obj;
+                    userDN += si.getName();
+                    userDN += "," + BASEDN;
+                } else {
+                    System.out.println(obj);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("查找用户时产生异常。");
+            e.printStackTrace();
+        }
+
+        return userDN;
+    }
+
+    public boolean authenricate(String UID, String password) {
+        boolean valide = false;
+        String userDN = getUserDN(UID);
+
+        try {
+            ctx.addToEnvironment(Context.SECURITY_PRINCIPAL, userDN);
+            ctx.addToEnvironment(Context.SECURITY_CREDENTIALS, password);
+            ctx.reconnect(connCtls);
+            System.out.println(userDN + " 验证通过");
+            valide = true;
+        } catch (AuthenticationException e) {
+            System.out.println(userDN + " 验证失败");
+            System.out.println(e.toString());
+            valide = false;
+        } catch (NamingException e) {
+            System.out.println(userDN + " 验证失败");
+            valide = false;
+        }
+
+        return valide;
+    }
+}
